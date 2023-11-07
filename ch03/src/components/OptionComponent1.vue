861571e020f7d464043c7a701c80bc127b9fe99c
lomong7807 <lomong7807@gmail.com>
2023-11-07 11:02:08 +0900

    option 수업

---
+++ ch03/src/components/OptionComponent1.vue
@@ -0,0 +1,23 @@
+<script>
+export default {
+  name: "OptionComponent1",
+
+  // 컴포넌트에서 사용할 데이터(상태값) 선언 속성
+  data() {
+    return {
+      name: "홍길동",
+      age: 21,
+      addr: "부산",
+    };
+  },
+};
+</script>
+
+<template>
+  <h4>data 옵션</h4>
+  <p>이름 : {{ name }} <br /></p>
+  <p>이름 : {{ age }} <br /></p>
+  <p>이름 : {{ addr }} <br /></p>
+</template>
+
+<style scoped></style>

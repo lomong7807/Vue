65d31baf72702b6fa35dbbe0a77dde6c76f3f3d9
lomong7807 <lomong7807@gmail.com>
2023-11-13 11:11:13 +0900

    todos

---
+++ ch06/src/App.vue
@@ -0,0 +1,35 @@
+<script setup>
+import { useMainStore } from "./store/mainStore";
+import MyComponent1 from "./components/Mycomponent1.vue";
+import Mycomponent2 from "./components/MyComponent2.vue";
+
+const mainStore = useMainStore();
+</script>
+
+<template>
+  <h3>ch06.Pinia 실습</h3>
+
+  <p>
+    mainStore title : {{ mainStore.title }}<br />
+    mainStore title : {{ mainStore.count }}<br />
+  </p>
+  <p>cities : {{ mainStore.cities }}</p>
+
+  <table border="1">
+    <tr>
+      <td>아이디</td>
+      <td>이름</td>
+      <td>나이</td>
+    </tr>
+    <tr v-for="person in mainStore.persons">
+      <td>{{ person.uid }}</td>
+      <td>{{ person.name }}</td>
+      <td>{{ person.age }}</td>
+    </tr>
+  </table>
+
+  <MyComponent1></MyComponent1>
+  <Mycomponent2></Mycomponent2>
+</template>
+
+<style scoped></style>
